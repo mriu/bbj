edfdf30c279efe7061821cc4930efc0af901fd03
ajmoir@indiana.edu <ajmoir@indiana.edu>
2013-04-23 14:48:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbj::Application.config.secret_token = '5952924bb0a7b125b689ffa14d04d0e94b58ed8f1418b5d02a097ad43d55cbbc3cd85c098f02c5b234d45f59146eb7c148fafeeab3029477b17311fafb1b9cc3'
